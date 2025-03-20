23b88f7c68842bc692ec0bd4714e833eb8aeefef
CristianTobar07 <cristianndres@unicauca.edu.co>
2025-03-20 18:32:30 -0500

    feat: update stiles in start

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-inicio',
-  imports: [CommonModule],
+  imports: [IonicModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.scss'
 })
